01ebcfac1026d78ad84d3fadbcefd2f8d5ca5b00
marekk1992 <m.komaisko@gmail.com>
2022-01-21 22:49:05 +0200

    removed brackets from line 8

--- src/exercises/decimalComparator/DecimalComparator.java
+++ src/exercises/decimalComparator/DecimalComparator.java
@@ -5,6 +5,6 @@ public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
         int castNumber1 = (int) (number1 * 1000);
         int castNumber2 = (int) (number2 * 1000);
-        return (castNumber1 == castNumber2);
+        return castNumber1 == castNumber2;
     }
 }
