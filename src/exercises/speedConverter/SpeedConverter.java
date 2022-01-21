bd7b3d06b15a29500cb979e5d9d7b207b793c5d8
marekk1992 <m.komaisko@gmail.com>
2022-01-21 21:55:14 +0200

    add printConversation method to program

--- src/exercises/speedConverter/SpeedConverter.java
+++ src/exercises/speedConverter/SpeedConverter.java
@@ -9,4 +9,13 @@ public class SpeedConverter {
             return Math.round(kilometersPerHour / 1.609);
         }
     }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = "
+                    + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
 }
