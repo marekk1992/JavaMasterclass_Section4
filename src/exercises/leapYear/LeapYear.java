c82a11da9703615654dbca238b8c1275339c391a
marekk1992 <m.komaisko@gmail.com>
2022-01-21 18:01:20 +0200

    add leapYear method

---
+++ src/exercises/leapYear/LeapYear.java
@@ -0,0 +1,19 @@
+package exercises.leapYear;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if ((year % 4) == 0) {
+                if ((year % 100) != 0) {
+                    return true;
+                } else {
+                    if ((year % 400) == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
