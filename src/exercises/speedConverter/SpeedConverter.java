81b6f9a3dbdbdc404af807bfc728d744ce9c6694
marekk1992 <m.komaisko@gmail.com>
2022-01-21 17:53:09 +0200

    add speedConverter program

---
+++ src/exercises/speedConverter/SpeedConverter.java
@@ -0,0 +1,12 @@
+package exercises.speedConverter;
+
+public class SpeedConverter {
+
+    public static double toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+}
