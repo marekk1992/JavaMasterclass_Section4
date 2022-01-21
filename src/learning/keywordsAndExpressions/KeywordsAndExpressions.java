d258b745bbb9324ca2c5bb25954e2e8cada8ab90
marekk1992 <m.komaisko@gmail.com>
2022-01-21 17:40:07 +0200

    add learning package and .gitignore file

---
+++ src/learning/keywordsAndExpressions/KeywordsAndExpressions.java
@@ -0,0 +1,14 @@
+package learning.keywordsAndExpressions;
+
+public class KeywordsAndExpressions {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
