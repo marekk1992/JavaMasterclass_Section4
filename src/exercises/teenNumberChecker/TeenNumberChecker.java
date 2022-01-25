946a0810d82e9c453f90d8bd3147a964947d5d2f
marekk1992 <m.komaisko@gmail.com>
2022-01-25 12:36:46 +0200

    added isTeen method to hasTeen method

--- src/exercises/teenNumberChecker/TeenNumberChecker.java
+++ src/exercises/teenNumberChecker/TeenNumberChecker.java
@@ -3,8 +3,7 @@ package exercises.teenNumberChecker;
 public class TeenNumberChecker {
 
     public static boolean hasTeen(int value1, int value2, int value3) {
-        return (value1 <= 19 && value1 >= 13) || (value2 <= 19 && value2 >= 13)
-                || (value3 <= 19 && value3 >= 13);
+        return isTeen(value1) || isTeen(value2) || isTeen(value3);
     }
 
     public static boolean isTeen(int value) {
